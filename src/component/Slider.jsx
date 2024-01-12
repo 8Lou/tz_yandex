507a7bb86973f9ed3458bf04b3841c584b0224c7
8Lou <lou8@mail.ru>
2024-01-12 21:25:36 +0300

    arrow_style

--- src/component/Slider.jsx
+++ src/component/Slider.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import styled from 'styled-components';
 import mockData from '../assets/mockData.ts';
 
 const SliderWrapper = styled.div`
   display: flex;
-  overflow-x: scroll;
+  position: relative;
+  overflow: hidden;
+`;
+const SliderContent = styled.div`
+  display: flex;
+  overflow-x: hidden;
+  scroll-snap-type: x mandatory;
   scroll-behavior: smooth;
 `;
 
@@ -15,6 +21,7 @@ const Slide = styled.div`
   position: relative;
   // border-radius: 20px;
   background-color: #f2f2f2;
+  
 
   img {
     width: 100%;
@@ -23,12 +30,58 @@ const Slide = styled.div`
     z-index: 1;
     object-fit: cover;
     clip-path: ${props => props.clipPath};
+    object-fit: cover;
   }
 
   div {
     padding: 10px;
   }
 `;
+
+const SlideControls = styled.div`
+  position: absolute;
+  bottom: -16px;
+  // left: 5%;
+  // transform: translateX(-50%);
+  // top: 50%;
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  // padding: 0 20px;
+  // box-sizing: border-box;
+  left: 0;
+  right: 0;
+`;
+
+const PrevSlideButton = styled.button`
+  width: 5%; 
+  height: 50px;
+  background: none;
+  border: none;
+  cursor: pointer;
+  position: relative;
+`;
+
+const NextSlideButton = styled.button`
+  width: 200px;
+  height: 50px;
+  background: none;
+  border: none;
+  cursor: pointer;
+  position: relative;
+`;
+
+const LeftArrowIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#7479A1" width="28px" height="50px">
+    <path d="M0 12l10-8v6h14v4h-14v6z" />
+  </svg>
+);
+
+const RightArrowIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#7479A1" width="28px" height="50px">
+    <path d="M24 12l-10-8v6h-14v4h14v6z" />
+  </svg>
+);
 
 const Slider = () => {
   const generateClipPath = (index) => {
@@ -41,17 +94,40 @@ const Slider = () => {
     }
   };
 
+  const sliderRef = useRef();
+
+  const scrollToPrev = () => {
+    sliderRef.current.scrollBy({
+      left: -sliderRef.current.offsetWidth,
+      behavior: 'smooth'
+    });
+  };
+
+  const scrollToNext = () => {
+    sliderRef.current.scrollBy({
+      left: sliderRef.current.offsetWidth,
+      behavior: 'smooth'
+    });
+  };
+
   return (
     <SliderWrapper>
-      {mockData.map((item, index) => (
-        <Slide key={item.id} clipPath={generateClipPath(index)}>
-          <img src={item.img} alt={item.title} />
-          <div>
-            <h3>{item.title}</h3>
-            <p>{item.date}</p>
-          </div>
-        </Slide>
-      ))}
+      <SliderContent ref={sliderRef}>
+
+        {mockData.map((item, index) => (
+          <Slide key={item.id} clipPath={generateClipPath(index)}>
+            <img src={item.img} alt={item.title} />
+            <div>
+              <h3>{item.title}</h3>
+              <p>{item.date}</p>
+            </div>
+          </Slide>
+        ))}
+      </SliderContent>
+      <SlideControls>
+        <PrevSlideButton onClick={scrollToPrev}><LeftArrowIcon /></PrevSlideButton>
+        <NextSlideButton onClick={scrollToNext}><RightArrowIcon /></NextSlideButton>
+      </SlideControls>
     </SliderWrapper>
   );
 };
